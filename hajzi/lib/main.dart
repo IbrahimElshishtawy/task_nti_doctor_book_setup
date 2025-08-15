2079a8230da45521c181a7d620cac0e5205ad3d8
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-08-15 23:13:07 +0300

    add image and create homepage

--- hajzi/lib/main.dart
+++ hajzi/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hajzi/pages/auth/Register_page.dart';
+import 'package:hajzi/pages/home/home_page.dart';
 
 // Cubits
 import 'cubits/auth/auth_cubit.dart';
@@ -56,7 +57,7 @@ class HajziApp extends StatelessWidget {
           '/onboarding': (context) => const OnboardingPage(),
           '/login': (context) => LoginPage(),
           '/register': (context) => const RegisterPage(),
-          // '/home': (context) => const HomePage(),
+          '/home': (context) => const HomePage(),
         },
       ),
     );
